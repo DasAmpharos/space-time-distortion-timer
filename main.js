de8da7e14a2ea397fd049e9da86cf7638da63853
Dylan Meadows <dylan_r_meadows@homedepot.com>
2022-02-28 16:46:18 -0500

    WIP js progress

--- main.js
+++ main.js
@@ -1,72 +1,102 @@
-var paused = false;
-var running = false;
-var timerHandle = -1;
-var lastMark = null;
+let paused = false;
+let running = false;
+let timerHandle = -1;
+let lastMark = null;
 
-var elapsed = 0;
-var totalElapsed = 0;
+let elapsed = 0;
+let totalElapsed = 0;
+let stageIdx = 0;
 
-var timerBtn = document.getElementById("timerBtn");
-var pauseBtn = document.getElementById("pauseBtn");
-var remainingLbl = document.getElementById("remainingLbl");
-var totalElapsedLbl = document.getElementById("totalElapsedLbl");
+let timerBtn = document.getElementById("timerBtn");
+let pauseBtn = document.getElementById("pauseBtn");
+let resetBtn = document.getElementById("resetBtn");
+let remainingLbl = document.getElementById("remainingLbl");
+let totalElapsedLbl = document.getElementById("totalElapsedLbl");
+
+const stages = Object.freeze([5, 10, 15, 25, 40].map(it => it * 60000));
+
+class TimeMark {
+    #mark = new Date();
+
+    elapsedNow() {
+        let now = new Date();
+        return now.getTime() - this.#mark.getTime();
+    }
+}
 
 timerBtn.onclick = () => {
-    if (!running) {
-        start();
-     } else {
-         stop();
-     }
+    (!running ? start : stop)();
 };
 
 pauseBtn.onclick = () => {
-    if (!paused) {
-        pause();
-    } else {
-        resume();
-    }
+    (!paused ? pause : resume)();
 };
+
+resetBtn.onclick = reset;
 
 function start() {
     running = true;
-    lastMark = new Date();
-    timerBtn.innerHTML = "Stop"
-
+    lastMark = new TimeMark();
     timerHandle = window.setInterval(() => {
-        var now = new Date();
-        var delta = now.getTime() - lastMark.getTime();
-        totalElapsedLbl.innerHTML = totalElapsed + delta;
+        let delta = lastMark.elapsedNow();
+        totalElapsedLbl.innerHTML = formatTime(totalElapsed + delta);
         if (!paused) {
-            remainingLbl.innerHTML = elapsed + delta;
+            let remaining = stages[stageIdx] - (elapsed + delta);
+            remainingLbl.innerHTML = formatTime(remaining);
+            if (remaining <= 0) {
+                stageIdx++;
+                if (stageIdx >= stages.length) {
+                    stop(); reset();
+                }
+            }
         }
     }, 1000);
 
+    timerBtn.innerHTML = "Stop"
     pauseBtn.disabled = false;
 }
 
 function stop() {
     running = false;
     window.clearInterval(timerHandle);
-    totalElapsedLbl.innerHTML = 0;
-    remainingLbl.innerHTML = 0;
+    let delta = lastMark.elapsedNow();
+    totalElapsed += delta;
+    elapsed += delta;
 
     timerBtn.innerHTML = "Start";
     pauseBtn.disabled = true;
 }
 
+function reset() {
+    lastMark = new TimeMark();
+    stageIdx = 0;
+    totalElapsed = 0;
+    elapsed = 0;
+
+    totalElapsedLbl.innerHTML = formatTime(0);
+    remainingLbl.innerHTML = formatTime(stages[stageIdx]);
+}
+
 function pause() {
     paused = true;
-    var now = new Date();
-    elapsed += now.getTime() - lastMark.getTime()
+    elapsed += lastMark.elapsedNow();
+
     pauseBtn.innerHTML = "Resume";
 }
 
 function resume() {
     paused = false;
-    lastMark = new Date();
+    totalElapsed += lastMark.elapsedNow();
+    lastMark = new TimeMark();
+
     pauseBtn.innerHTML = "Pause";
 }
 
-function reset() {
-
+function formatTime(milliseconds) {
+    let asSeconds = Math.round(milliseconds / 1000);
+    let minutesPart = Math.floor(asSeconds / 60);
+    let secondsPart = (asSeconds % 60).toString().padStart(2, '0');
+    return `${minutesPart}:${secondsPart}`;
 }
+
+reset();
